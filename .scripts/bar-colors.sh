87bb49e1d4e19f9add4856c8912f9ca14ea86dd0
Dave Nicholson <fatnic388@gmail.com>
2018-05-11 20:38:09 +0100

    some bar stuff

---
+++ .scripts/bar-colors.sh
@@ -0,0 +1,13 @@
+BG="#cc151515"
+FG="#eeffffff"
+
+ACTIVE="#92b8cf"
+INACTIVE="#777777"
+OCCUPIED="#eeffffff"
+
+BLUE="#76b6da"
+RED="#c6726c"
+YELLOW="#c0c66c"
+GREEN="#b5e3b8"
+DARKGREY="#959595"
+PURPLE="#e3b5e0"
